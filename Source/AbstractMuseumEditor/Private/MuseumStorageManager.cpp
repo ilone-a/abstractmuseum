cb47c93f913809a81c8a3b316750ca11dfa2b567
ilone-a <10278825+ilone-a@users.noreply.github.com>
2026-01-29 00:35:01 +0300

    Create storage and show data tables

--- Source/AbstractMuseumEditor/Private/MuseumStorageManager.cpp
+++ Source/AbstractMuseumEditor/Private/MuseumStorageManager.cpp
@@ -2,7 +2,7 @@
 #include "Engine/World.h"
 #include "Kismet/GameplayStatics.h"
 #include "AbstractMuseumActor.h"
-
+#include "Misc/Paths.h"
 #include "Runtime/AssetRegistry/Public/AssetRegistry/AssetRegistryModule.h"
 
 
@@ -16,8 +16,7 @@ void FMuseumStorageManager::RebuildAMShow()
 		if (!EditorWorld) return;
 
 		//TODO remove hardcode
-		FString ShowTablePath = "/AbstractMuseum/Data/";
-		UDataTable* Table = GetOrCreateDataTable(ShowTablePath, "Show");
+		UDataTable* Table = GetOrCreateDataTable("/Game/Data", "Show");
 		if (!Table) return;
 
 		Table->Modify();
@@ -48,8 +47,8 @@ void FMuseumStorageManager::RebuildAMShow()
 void FMuseumStorageManager::RebuildAMStorage()
 {
 	//TODO remove hardcode
-	FString ShowTablePath = "/AbstractMuseum/Data/";
-	UDataTable* Table = GetOrCreateDataTable(ShowTablePath, "Storage");
+
+	UDataTable* Table = GetOrCreateDataTable("/Game/Data", "Storage");
 	if (!Table) return;
 
 	Table->Modify();
@@ -57,32 +56,72 @@ void FMuseumStorageManager::RebuildAMStorage()
 
 	FAssetRegistryModule& ARM =
 		FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
+	IAssetRegistry& Registry = ARM.Get();
 
+	// 1. Collect all child classes of AbstractMuseumActor
+	TArray<FTopLevelAssetPath> BaseClasses;
+	BaseClasses.Add(AAbstractMuseumActor::StaticClass()->GetClassPathName());
+
+	TSet<FTopLevelAssetPath> DerivedClasses;
+	Registry.GetDerivedClassNames(BaseClasses, {}, DerivedClasses);
+
+	// 2. Get all BP
 	TArray<FAssetData> Assets;
-	const FTopLevelAssetPath BlueprintClassPath =
-		UBlueprint::StaticClass()->GetClassPathName();
-
-	ARM.Get().GetAssetsByClass(
-		BlueprintClassPath,
+	Registry.GetAssetsByClass(
+		UBlueprint::StaticClass()->GetClassPathName(),
 		Assets,
 		true
 	);
 
 	for (const FAssetData& Asset : Assets)
 	{
-		const FString ParentClass =
-			Asset.GetTagValueRef<FString>("ParentClass");
+		const FString GeneratedClassPath =
+			Asset.GetTagValueRef<FString>("GeneratedClass");
 
-		if (!ParentClass.Contains("AbstractMuseumActor"))
+		if (GeneratedClassPath.IsEmpty())
+			continue;
+
+		const FString CleanClassPath =
+			FPackageName::ExportTextPathToObjectPath(GeneratedClassPath);
+
+		const FTopLevelAssetPath ClassPath(CleanClassPath);
+		// 3. Check hierarchy
+		if (!DerivedClasses.Contains(ClassPath))
+			continue;
+		UClass* BPClass = LoadObject<UClass>(nullptr, *CleanClassPath);
+		if (!BPClass)
+			continue;
+
+		UClass* It = BPClass;
+		UClass* FoundCppParent = nullptr;
+
+		while (It)
+		{
+			// first C++ native parent class
+			if (It->ClassGeneratedBy == nullptr)
+			{
+				FoundCppParent = It;
+				break;
+			}
+
+			It = It->GetSuperClass();
+		}
+
+		if (!FoundCppParent)
+			continue;
+
+		if (!FoundCppParent->IsChildOf(AAbstractMuseumActor::StaticClass()))
 			continue;
 
 		FAMDataTableRowEntry Row;
 		Row.DisplayName = Asset.AssetName;
-		Row.ActorClass = nullptr; // todo resolve
+		Row.ActorClass = FoundCppParent;
+		//Row.ActorClass = nullptr; 
 		Row.AssetPath = Asset.ObjectPath.ToString();
 
 		Table->AddRow(Row.DisplayName, Row);
 	}
+
 }
 
 UDataTable* FMuseumStorageManager::GetDataTable(const FString& FolderPath, const FString& AssetName)
